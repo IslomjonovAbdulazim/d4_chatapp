d926eb5adf96c53e995d084490b023ccc052882b
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-21 19:05:12 +0500

    imp

--- lib/pages/auth/login/login_widgets.dart
+++ lib/pages/auth/login/login_widgets.dart
@@ -1,7 +1,7 @@
 part of 'imports.dart';
 
 class _WelcomeTexts extends StatelessWidget {
-  const _WelcomeTexts({super.key});
+  const _WelcomeTexts();
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +23,7 @@ class _WelcomeTexts extends StatelessWidget {
 }
 
 class _Inputs extends GetView<LoginController> {
-  const _Inputs({super.key});
+  const _Inputs();
 
   @override
   Widget build(BuildContext context) {
@@ -39,6 +39,81 @@ class _Inputs extends GetView<LoginController> {
           controller: controller.passwordController,
           focus: FocusNode(),
           hint: "Password",
+        ),
+      ],
+    );
+  }
+}
+
+class _ForgetPassword extends StatelessWidget {
+  const _ForgetPassword();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Spacer(),
+        CupertinoButton(
+          padding: EdgeInsets.zero,
+          onPressed: () {
+            Get.to(ForgetPasswordPage());
+          },
+          child: Text(
+            "Forget password?",
+            style: context.name,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Login extends GetView<LoginController> {
+  const _Login();
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(() {
+      return controller.isLoading.value == false
+          ? CupertinoButton(
+              color: context.textPrimary,
+              disabledColor: context.textPrimary,
+              onPressed: controller.login,
+              child: Center(
+                child: Text(
+                  "Login",
+                  style: context.name.copyWith(
+                    color: context.backgroundColor,
+                  ),
+                ),
+              ),
+            )
+          : CircularProgressIndicator();
+    });
+  }
+}
+
+class _GoRegister extends StatelessWidget {
+  const _GoRegister();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "New member?",
+          style: context.body,
+        ),
+        CupertinoButton(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+          onPressed: () {
+            Get.to(RegisterPage());
+          },
+          child: Text(
+            "Register Now",
+            style: context.name,
+          ),
         ),
       ],
     );

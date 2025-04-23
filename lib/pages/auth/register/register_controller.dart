6f60536f8f59d6666de11c84f96aef017c30a9fe
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-23 17:25:34 +0500

    imp

--- lib/pages/auth/register/register_controller.dart
+++ lib/pages/auth/register/register_controller.dart
@@ -1,3 +1,20 @@
 part of 'imports.dart';
 
-class RegisterController extends GetxController {}
+class RegisterController extends GetxController {
+  TextEditingController fullnameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  RxBool isLoading = false.obs;
+
+  void register() async {
+    isLoading.value = true;
+    String fullname = fullnameController.text.trim();
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+
+    // connect to backend
+
+    await Future.delayed(Duration(seconds: 1));
+    isLoading.value = false;
+  }
+}
